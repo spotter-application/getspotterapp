8d54790587b816e0bdeec9cd6258be185b377f5e
Denis Z <zzulev@gmail.com>
2021-03-14 21:48:17 +0100

    Update url

--- src/App.tsx
+++ src/App.tsx
@@ -94,7 +94,7 @@ function App() {
               </a>
 
               <div>
-                <a href="https://github.com/sponsors/jet8a" className="link link--bottom">
+                <a href="https://github.com/sponsors/ziulev" className="link link--bottom">
                   <svg aria-hidden="true" focusable="false" role="img" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg"><path d="M462.3 62.6C407.5 15.9 326 24.3 275.7 76.2L256 96.5l-19.7-20.3C186.1 24.3 104.5 15.9 49.7 62.6c-62.8 53.6-66.1 149.8-9.9 207.9l193.5 199.8c12.5 12.9 32.8 12.9 45.3 0l193.5-199.8c56.3-58.1 53-154.3-9.8-207.9z" fill="currentColor"></path></svg>
                   Sponsor
                 </a>
@@ -106,7 +106,7 @@ function App() {
 
       </section>
       <footer className="footer">
-        <a href="https://github.com/jet8a">© Denis Ziulev</a>
+        <a href="https://github.com/ziulev">© Denis Ziulev</a>
       </footer>
     </>
   );
